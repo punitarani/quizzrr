0ef967fb185c045e0ca024935554f5d174d93a9f
Punit Arani <punitsai36@gmail.com>
2024-05-23 11:12:01 -0700

    Add quiz router and remove post router

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,4 +1,4 @@
-import { postRouter } from "~/server/api/routers/post";
+import { quizRouter } from "~/server/api/routers/quiz";
 import { createCallerFactory, createTRPCRouter } from "~/server/api/trpc";
 
 /**
@@ -7,7 +7,7 @@ import { createCallerFactory, createTRPCRouter } from "~/server/api/trpc";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  post: postRouter,
+  quiz: quizRouter,
 });
 
 // export type definition of API
---
+++ src/server/api/routers/quiz.ts
@@ -0,0 +1,31 @@
+// src/server/api/routers/quiz.ts
+
+import { generateText } from "ai";
+import { z } from "zod";
+
+import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+
+import { llama3_70b } from "~/lib/llm";
+
+async function generateOutline(topic: string) {
+  const { text } = await generateText({
+    model: llama3_70b,
+    system:
+      "Generate a clear and concise outline for the given topic. " +
+      "It needs to only include the main topics without subtopics in logical order. " +
+      "Do not include any examples, explanations, or other details. " +
+      "Generate the response in pretty and simple markdown format without a title and using bullet points.",
+    prompt: topic,
+  });
+  console.log(`Generated outline for ${topic}: ${text}`);
+  return text;
+}
+
+export const quizRouter = createTRPCRouter({
+  outline: publicProcedure
+    .input(z.object({ topic: z.string() }))
+    .query(async ({ input }) => {
+      const outline = await generateOutline(input.topic);
+      return { outline: outline };
+    }),
+});
