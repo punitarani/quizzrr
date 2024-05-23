bfb7ff7b534325902df0b1a6bf0760a1c29c0adf
Punit Arani <punitsai36@gmail.com>
2024-05-23 06:35:08 -0700

    Init shadcn ui

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,15 @@ import { GeistSans } from "geist/font/sans";
 import { TRPCReactProvider } from "~/trpc/react";
 import React from "react";
 
+import { Inter as FontSans } from "next/font/google";
+
+import { cn } from "~/lib/utils";
+
+const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
+
 export const metadata = {
   title: "quizzrr",
   description: "quizzrr",
@@ -18,7 +27,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={`${GeistSans.variable}`}>
-      <body>
+      <body
+        className={cn(
+          "min-h-screen bg-background font-sans antialiased",
+          fontSans.variable,
+        )}
+      >
         <TRPCReactProvider>{children}</TRPCReactProvider>
       </body>
     </html>
