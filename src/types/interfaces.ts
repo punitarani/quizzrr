50cf9398595c5bb0c481639898dd66a00fbe27ba
Punit Arani <punitsai36@gmail.com>
2024-05-25 15:49:37 -0700

    Add Quiz types

---
+++ src/types/interfaces.ts
@@ -0,0 +1,38 @@
+// src/types/interfaces.ts
+
+import { z } from "zod";
+
+export const QuizInfoSchema = z.object({
+  topic: z.string(),
+  subject: z.string(),
+  level: z.string(),
+  length: z.string().optional(),
+});
+export type QuizInfoData = z.infer<typeof QuizInfoSchema>;
+
+export const QuizQuestionSchema = z.object({
+  question: z.string(),
+  description: z.string(),
+  difficulty: z.string(),
+});
+export type QuizQuestionData = z.infer<typeof QuizQuestionSchema>;
+
+export const QuizAnswerSchema = z.object({
+  userAnswer: z.string(),
+  correctAnswer: z.string(),
+  isCorrect: z.boolean(),
+  feedback: z.string(),
+});
+export type QuizAnswerData = z.infer<typeof QuizAnswerSchema>;
+
+export const QuizQuestionUserAnswerSchema = z.object({
+  question: z.string(),
+  description: z.string(),
+  userAnswer: z.string(),
+  correctAnswer: z.string(),
+  isCorrect: z.boolean(),
+  feedback: z.string(),
+});
+export type QuizQuestionUserAnswerData = z.infer<
+  typeof QuizQuestionUserAnswerSchema
+>;
