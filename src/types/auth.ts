55d0b2251a7b48c02586754f9db210abcfa73a26
Punit Arani <punitsai36@gmail.com>
2024-08-19 20:39:22 +0000

    Create GoogleCredentialResponse type

---
+++ src/types/auth.ts
@@ -0,0 +1,5 @@
+export interface GoogleCredentialResponse {
+  credential: string
+  select_by: string
+  client_id?: string
+}
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,4 @@
 // src/types/index.ts
 
+export * from './auth'
 export * from './interfaces'
